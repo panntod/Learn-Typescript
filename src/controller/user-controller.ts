9a3f50bc6c8ccd514b17b54ffa0e9b6b34af1410
Pandhu Arya <pandhu.munjalindra@gmail.com>
2024-02-09 05:13:51 +0700

    feat: added interface data and post new data

--- src/controller/user-controller.ts
+++ src/controller/user-controller.ts
@@ -1,33 +1,53 @@
-import { user as userModel } from "../models/index";
-import bcrypt from "bcrypt";
-import { Request, Response } from 'express';
+const bcrypt = require("bcrypt");
+const userModel = require("../models/index").user;
+import { Request, Response } from "express";
 
 export const getAllUser = async (req: Request, res: Response) => {
-    try {
-      const dataUser = await userModel.findAll();
-  
-      const responseUsers = dataUser.map((user: any) => {
-        const { id, firstname, lastname, email, role } = user;
-        return {
-          id,
-          firstname,
-          lastname,
-          email,
-          role,
-        };
-      });
-  
-      return res.status(200).json({
-        success: true,
-        data: responseUsers,
-        message: "All users have been loaded",
-      });
-    } catch (error) {
-      console.error("Error in getAllUser:", error);
-      return res.status(500).json({
-        success: false,
-        data: null,
-        message: "Internal Server Error",
-      });
-    }
+  try {
+    const dataUser = await userModel.findAll();
+
+    return res.status(200).json({
+      success: true,
+      data: dataUser,
+      message: "All users have been loaded",
+    });
+  } catch (error) {
+    console.error("Error in getAllUser:", error);
+    return res.status(500).json({
+      success: false,
+      data: null,
+      message: "Internal Server Error",
+    });
+  }
+};
+
+interface DataUser {
+  firstname: string;
+  lastname: string;
+  email: string;
+  role: string;
+  password: string;
+}
+
+export const registration = async (req: Request, res: Response) => {
+  let newUser: DataUser = {
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    email: req.body.email,
+    role: req.body.role,
+    password: bcrypt.hashSync(req.body.password, 10),
   };
+  try {
+    const result: any = await userModel.create(newUser)
+    return res.json({
+      success: true,
+      rowAffected: result,
+      message: "New member has been inserted",
+    });
+  } catch (error: any) {
+    return res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
