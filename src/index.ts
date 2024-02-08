ce859210d3e1b14630257241e566130809bee64c
Pandhu Arya <pandhu.munjalindra@gmail.com>
2024-02-09 05:13:27 +0700

    feat: added app use body-parser json and url encoded

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 const app = express();
 const port = 5000;
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
 
 import userRouter from './routes/user-routes';
 
