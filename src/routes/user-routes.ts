acb7d351ef1427b795716100453d32f43ca2adfc
Pandhu Arya <pandhu.munjalindra@gmail.com>
2024-02-09 05:15:10 +0700

    feat: deleted message, and added path to post data

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,9 +1,9 @@
-// user-routes.ts
 import express from 'express';
 const router = express.Router();
 
 import * as userController from '../controller/user-controller';
 
 router.get('/getAll', userController.getAllUser);
+router.post('/', userController.registration);
 
 export default router;
